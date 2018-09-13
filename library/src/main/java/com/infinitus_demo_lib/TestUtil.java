8dd3e531be155d055d5f3c0332b4367558dccbd1
ex_huangwc <>
2018-09-13 22:56:11 +0800

    Signed-off-by: ex_huangwc <黄伟才>

---
+++ library/src/main/java/com/infinitus_demo_lib/TestUtil.java
@@ -0,0 +1,17 @@
+package com.infinitus_demo_lib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 时间：2018/9/13
+ * 作者：蓝天
+ * 描述：
+ */
+public class TestUtil {
+
+    public static void test1(Context context) {
+        Toast.makeText(context, "我是jcenter中的lib", Toast.LENGTH_LONG).show();
+    }
+
+}
