ef5f0a7845ce28a64640a16c2716985e795aa5af
ex_huangwc <>
2018-09-17 16:45:36 +0800

    Signed-off-by: ex_huangwc <黄伟才>

--- app/src/main/java/yuan/hua/com/myjcenter/MainActivity.java
+++ app/src/main/java/yuan/hua/com/myjcenter/MainActivity.java
@@ -3,12 +3,14 @@ package yuan.hua.com.myjcenter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.infinitus_demo_lib.TestUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TestUtil.test1(this);
     }
 }
